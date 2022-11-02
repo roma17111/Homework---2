79986effb658a177d9cbd07ffd7de4a2ac3d177f
Роман <romanze1706@gmail.com>
2022-11-02 14:46:34 +0300

    проверьте задание 5

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,14 @@ friend = friend + 2;
 System.out.println(friend);
 friend = friend / 7;
 System.out.println(friend);
-
+var frog = 3.5;
+System.out.println(frog);
+frog = frog * 10;
+System.out.println(frog);
+frog = frog / 3.5;
+System.out.println(frog);
+frog = frog + 4;
+System.out.println(frog);
 
     }
 }
