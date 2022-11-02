c65e352c3091db349977443099878cca2c87fd77
Роман <romanze1706@gmail.com>
2022-11-02 14:32:46 +0300

    проверьте задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+var friend = 19;
+System.out.println(friend);
+friend = friend + 2;
+System.out.println(friend);
+friend = friend / 7;
+System.out.println(friend);
 
     }
 }
